9016fb85de40ed001d28726d1297b30ba13582ab
Zydell <cristhiantotoymorales@gmail.com>
2024-05-01 17:06:47 -0500

    qwe

--- test.py
+++ test.py
@@ -1,34 +1,5 @@
+def days_to_minutes(xd):
+    return xd * 24 * 60
 
-class Stack:
-    def __init__(self):
-        self.stack = []
-
-    def push(self, item):
-        self.stack.append(item)
-
-    def pop(self):
-        if len(self.stack) < 1:
-            return None
-        return self.stack.pop()
-
-    def sort_and_push(self, item):
-        if not self.stack:
-            self.push(item)
-        else:
-            temp_stack = []
-            while self.stack and self.stack[-1] < item:
-                temp_stack.append(self.stack.pop())
-            self.push(item)
-            while temp_stack:
-                self.push(temp_stack.pop())
-
-# Example usage:
-stack = Stack()
-stack.sort_and_push('juan')
-stack.sort_and_push('mariano')
-stack.sort_and_push('arabic')
-stack.sort_and_push('peter')
-
-
-while stack.stack:
-    print(stack.pop())
+res=days_to_minutes(5)
+print(res)
