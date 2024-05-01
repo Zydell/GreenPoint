a0d628e3a044abbc884ea99d0ccbf4a1681f8027
Zydell <cristhiantotoymorales@gmail.com>
2024-05-01 16:53:10 -0500

    test

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 name = 'XD'
 na = name+'DSA'
 print(name)
+
+for i in range(10):  # Change the range as needed
+    for j in range(i+1, 10):  # Change the range as needed
+        print(f"({i}, {j})")
