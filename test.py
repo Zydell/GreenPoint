83978a6e9637f0782d6d7389c8ede03cc34e736d
Zydell <cristhiantotoymorales@gmail.com>
2024-05-01 17:25:52 -0500

    changes

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-def days_to_minutes(xd):
-    return xd * 24 * 60
 
-res=days_to_minutes(5)
-print(res)
+def months_to_minutes(months):
+    # There are approximately 30.44 days in a month on average (365.25/12)
+    # There are 24 hours in a day
+    # There are 60 minutes in an hour
+    minutes_per_month = 30.44 * 24 * 60
+    return int(months * minutes_per_month)
+
+print(months_to_minutes(6))
+
