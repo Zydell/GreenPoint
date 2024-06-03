2b1949856115929bf22c13464c240bb3404cd4af
Cristhian <120286539+Zydell@users.noreply.github.com>
2024-06-03 13:49:36 -0500

    Act

--- Documentation/db_greenpoint.sql
+++ Documentation/db_greenpoint.sql
@@ -1,37 +1,37 @@
 /*==============================================================*/
 /* DBMS name:      PostgreSQL 8                                 */
-/* Created on:     3/6/2024 13:40:50                            */
+/* Created on:     3/6/2024 13:49:06                            */
 /*==============================================================*/
 
 
-drop table admin;
+drop table tb_admin;
 
-drop table canjea_oferta;
+drop table tb_canjea_oferta;
 
-drop table ciudadano;
+drop table tb_ciudadano;
 
-drop table credenciales;
+drop table tb_credenciales;
 
-drop table greencoin_cdn;
+drop table tb_greencoin_cdn;
 
-drop table historial_reciclaje_ciudadano;
+drop table tb_historial_cdn;
 
-drop table historial_reciclaje_negocio;
+drop table tb_historial_negocio;
 
-drop table materiales;
+drop table tb_materiales;
 
-drop table negocio;
+drop table tb_negocio;
 
-drop table ofertas;
+drop table tb_ofertas;
 
-drop table puntos_verdes;
+drop table tb_puntos_verdes;
 
-drop table reciclaje;
+drop table tb_reciclaje;
 
-drop table registra_reciclaje;
+drop table tb_registra_reciclaje;
 
 /*==============================================================*/
-/* Table: admin                                                 */
+/* Table: tb_admin                                              */
 /*==============================================================*/
 create table tb_admin (
    admin_id             SERIAL               not null,
@@ -42,22 +42,22 @@ create table tb_admin (
    nombre               VARCHAR(25)          null,
    correo               VARCHAR(50)          null,
    password             VARCHAR(25)          null,
-   constraint PK_ADMIN primary key (admin_id)
+   constraint PK_TB_ADMIN primary key (admin_id)
 );
 
 /*==============================================================*/
-/* Table: canjea_oferta                                         */
+/* Table: tb_canjea_oferta                                      */
 /*==============================================================*/
 create table tb_canjea_oferta (
    canjeo_id            SERIAL               not null,
    ofertas_id           INT4                 null,
    ciudadano_id         INT4                 null,
    fecha                DATE                 null,
-   constraint PK_CANJEA_OFERTA primary key (canjeo_id)
+   constraint PK_TB_CANJEA_OFERTA primary key (canjeo_id)
 );
 
 /*==============================================================*/
-/* Table: ciudadano                                             */
+/* Table: tb_ciudadano                                          */
 /*==============================================================*/
 create table tb_ciudadano (
    ciudadano_id         SERIAL               not null,
@@ -67,11 +67,11 @@ create table tb_ciudadano (
    telefono             CHAR(10)             not null,
    fecha_nac            DATE                 not null,
    fecharegistro        DATE                 not null,
-   constraint PK_CIUDADANO primary key (ciudadano_id)
+   constraint PK_TB_CIUDADANO primary key (ciudadano_id)
 );
 
 /*==============================================================*/
-/* Table: credenciales                                          */
+/* Table: tb_credenciales                                       */
 /*==============================================================*/
 create table tb_credenciales (
    credencial_id        SERIAL               not null,
@@ -79,24 +79,24 @@ create table tb_credenciales (
    contrasena           VARCHAR(100)         not null,
    tipousuario          INT4                 not null,
    usuario_id           INT4                 not null,
-   constraint PK_CREDENCIALES primary key (credencial_id)
+   constraint PK_TB_CREDENCIALES primary key (credencial_id)
 );
 
 /*==============================================================*/
-/* Table: greencoin_cdn                                         */
+/* Table: tb_greencoin_cdn                                      */
 /*==============================================================*/
 create table tb_greencoin_cdn (
    greencoin_id         SERIAL               not null,
    registro_id          INT4                 null,
    canjeo_id            INT4                 null,
    total                INT4                 null,
-   constraint PK_GREENCOIN_CDN primary key (greencoin_id)
+   constraint PK_TB_GREENCOIN_CDN primary key (greencoin_id)
 );
 
 /*==============================================================*/
-/* Table: historial_reciclaje_ciudadano                         */
+/* Table: tb_historial_cdn                                      */
 /*==============================================================*/
-create table tb_hist_reciclaje_cdn (
+create table tb_historial_cdn (
    historial_id         SERIAL               not null,
    ciudadano_id         INT4                 null,
    punto_verde_id       INT4                 not null,
@@ -104,34 +104,34 @@ create table tb_hist_reciclaje_cdn (
    cantidad             FLOAT8               not null,
    fecha                DATE                 not null,
    green_coins_obtenidos INT4                 not null,
-   constraint PK_HISTORIAL_RECICLAJE_CIUDADA primary key (historial_id)
+   constraint PK_TB_HISTORIAL_CDN primary key (historial_id)
 );
 
 /*==============================================================*/
-/* Table: historial_reciclaje_negocio                           */
+/* Table: tb_historial_negocio                                  */
 /*==============================================================*/
-create table tb_hist_reciclaje_ngc (
+create table tb_historial_negocio (
    historial_id         SERIAL               not null,
    negocio_id           INT4                 not null,
    punto_verde_id       INT4                 not null,
    reciclaje_id         INT4                 not null,
    cantidad             FLOAT8               not null,
    fecha                DATE                 not null,
-   constraint PK_HISTORIAL_RECICLAJE_NEGOCIO primary key (historial_id)
+   constraint PK_TB_HISTORIAL_NEGOCIO primary key (historial_id)
 );
 
 /*==============================================================*/
-/* Table: materiales                                            */
+/* Table: tb_materiales                                         */
 /*==============================================================*/
 create table tb_materiales (
    materiales_id        SERIAL               not null,
    tipo                 VARCHAR(100)         not null,
    valor_por_libra      INT4                 not null,
-   constraint PK_MATERIALES primary key (materiales_id)
+   constraint PK_TB_MATERIALES primary key (materiales_id)
 );
 
 /*==============================================================*/
-/* Table: negocio                                               */
+/* Table: tb_negocio                                            */
 /*==============================================================*/
 create table tb_negocio (
    negocio_id           SERIAL               not null,
@@ -141,11 +141,11 @@ create table tb_negocio (
    direccion            VARCHAR(200)         not null,
    telefono             CHAR(10)             not null,
    fecharegistro        DATE                 not null,
-   constraint PK_NEGOCIO primary key (negocio_id)
+   constraint PK_TB_NEGOCIO primary key (negocio_id)
 );
 
 /*==============================================================*/
-/* Table: ofertas                                               */
+/* Table: tb_ofertas                                            */
 /*==============================================================*/
 create table tb_ofertas (
    ofertas_id           SERIAL               not null,
@@ -154,11 +154,11 @@ create table tb_ofertas (
    negocio_id           INT4                 not null,
    fecha_inicio         DATE                 not null,
    fecha_fin            DATE                 not null,
-   constraint PK_OFERTAS primary key (ofertas_id)
+   constraint PK_TB_OFERTAS primary key (ofertas_id)
 );
 
 /*==============================================================*/
-/* Table: puntos_verdes                                         */
+/* Table: tb_puntos_verdes                                      */
 /*==============================================================*/
 create table tb_puntos_verdes (
    punto_verde_id       SERIAL               not null,
@@ -167,21 +167,21 @@ create table tb_puntos_verdes (
    latitud              FLOAT8               not null,
    longitud             FLOAT8               not null,
    negocio_id           INT4                 not null,
-   constraint PK_PUNTOS_VERDES primary key (punto_verde_id)
+   constraint PK_TB_PUNTOS_VERDES primary key (punto_verde_id)
 );
 
 /*==============================================================*/
-/* Table: reciclaje                                             */
+/* Table: tb_reciclaje                                          */
 /*==============================================================*/
 create table tb_reciclaje (
    reciclaje_id         SERIAL               not null,
    material_id          INT4                 not null,
    cantidad             FLOAT8               not null,
-   constraint PK_RECICLAJE primary key (reciclaje_id)
+   constraint PK_TB_RECICLAJE primary key (reciclaje_id)
 );
 
 /*==============================================================*/
-/* Table: registra_reciclaje                                    */
+/* Table: tb_registra_reciclaje                                 */
 /*==============================================================*/
 create table tb_registra_reciclaje (
    registro_id          SERIAL               not null,
@@ -192,126 +192,126 @@ create table tb_registra_reciclaje (
    cantidad             FLOAT8               not null,
    fecha                DATE                 not null,
    gc_obtenidos         INT4                 not null,
-   constraint PK_REGISTRA_RECICLAJE primary key (registro_id)
+   constraint PK_TB_REGISTRA_RECICLAJE primary key (registro_id)
 );
 
 alter table tb_admin
-   add constraint FK_ADMIN_REFERENCE_CREDENCI foreign key (credencial_id)
-      references credenciales (credencial_id)
+   add constraint FK_TB_ADMIN_REFERENCE_TB_CREDE foreign key (credencial_id)
+      references tb_credenciales (credencial_id)
       on delete restrict on update restrict;
 
 alter table tb_admin
-   add constraint FK_ADMIN_REFERENCE_OFERTAS foreign key (ofertas_id)
-      references ofertas (ofertas_id)
+   add constraint FK_TB_ADMIN_REFERENCE_TB_OFERT foreign key (ofertas_id)
+      references tb_ofertas (ofertas_id)
       on delete restrict on update restrict;
 
-alter table admin
-   add constraint FK_ADMIN_REFERENCE_PUNTOS_V foreign key (punto_verde_id)
-      references puntos_verdes (punto_verde_id)
+alter table tb_admin
+   add constraint FK_TB_ADMIN_REFERENCE_TB_PUNTO foreign key (punto_verde_id)
+      references tb_puntos_verdes (punto_verde_id)
       on delete restrict on update restrict;
 
-alter table admin
-   add constraint FK_ADMIN_REFERENCE_NEGOCIO foreign key (negocio_id)
-      references negocio (negocio_id)
+alter table tb_admin
+   add constraint FK_TB_ADMIN_REFERENCE_TB_NEGOC foreign key (negocio_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table canjea_oferta
-   add constraint FK_CANJEA_O_REFERENCE_OFERTAS foreign key (ofertas_id)
-      references ofertas (ofertas_id)
+alter table tb_canjea_oferta
+   add constraint FK_TB_CANJE_REFERENCE_TB_OFERT foreign key (ofertas_id)
+      references tb_ofertas (ofertas_id)
       on delete restrict on update restrict;
 
-alter table canjea_oferta
-   add constraint FK_CANJEA_O_REFERENCE_CIUDADAN foreign key (ciudadano_id)
-      references ciudadano (ciudadano_id)
+alter table tb_canjea_oferta
+   add constraint FK_TB_CANJE_REFERENCE_TB_CIUDA foreign key (ciudadano_id)
+      references tb_ciudadano (ciudadano_id)
       on delete restrict on update restrict;
 
-alter table ciudadano
-   add constraint FK_CIUDADAN_REFERENCE_GREENCOI foreign key (greencoin_id)
-      references greencoin_cdn (greencoin_id)
+alter table tb_ciudadano
+   add constraint FK_TB_CIUDA_REFERENCE_TB_GREEN foreign key (greencoin_id)
+      references tb_greencoin_cdn (greencoin_id)
       on delete restrict on update restrict;
 
-alter table credenciales
-   add constraint FK_CREDENCI_REFERENCE_CIUDADAN foreign key (usuario_id)
-      references ciudadano (ciudadano_id)
+alter table tb_credenciales
+   add constraint FK_TB_CREDE_REFERENCE_TB_CIUDA foreign key (usuario_id)
+      references tb_ciudadano (ciudadano_id)
       on delete restrict on update restrict;
 
-alter table credenciales
-   add constraint FK_CREDENCI_REFERENCE_NEGOCIO foreign key (usuario_id)
-      references negocio (negocio_id)
+alter table tb_credenciales
+   add constraint FK_TB_CREDE_REFERENCE_TB_NEGOC foreign key (usuario_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table greencoin_cdn
-   add constraint FK_GREENCOI_REFERENCE_REGISTRA foreign key (registro_id)
-      references registra_reciclaje (registro_id)
+alter table tb_greencoin_cdn
+   add constraint FK_TB_GREEN_REFERENCE_TB_REGIS foreign key (registro_id)
+      references tb_registra_reciclaje (registro_id)
       on delete restrict on update restrict;
 
-alter table greencoin_cdn
-   add constraint FK_GREENCOI_REFERENCE_CANJEA_O foreign key (canjeo_id)
-      references canjea_oferta (canjeo_id)
+alter table tb_greencoin_cdn
+   add constraint FK_TB_GREEN_REFERENCE_TB_CANJE foreign key (canjeo_id)
+      references tb_canjea_oferta (canjeo_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_ciudadano
-   add constraint FK_HISTORIA_REFERENCE_CIUDADAN foreign key (ciudadano_id)
-      references ciudadano (ciudadano_id)
+alter table tb_historial_cdn
+   add constraint FK_TB_HISTO_REFERENCE_TB_CIUDA foreign key (ciudadano_id)
+      references tb_ciudadano (ciudadano_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_ciudadano
-   add constraint FK_HISTORIA_REFERENCE_PUNTOS_V foreign key (punto_verde_id)
-      references puntos_verdes (punto_verde_id)
+alter table tb_historial_cdn
+   add constraint FK_TB_HISTO_REFERENCE_TB_PUNTO foreign key (punto_verde_id)
+      references tb_puntos_verdes (punto_verde_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_ciudadano
-   add constraint FK_HISTORIA_REFERENCE_RECICLAJ foreign key (reciclaje_id)
-      references reciclaje (reciclaje_id)
+alter table tb_historial_cdn
+   add constraint FK_TB_HISTO_REFERENCE_TB_RECIC foreign key (reciclaje_id)
+      references tb_reciclaje (reciclaje_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_negocio
-   add constraint FK_HISTORIA_REFERENCE_PUNTOS_V foreign key (punto_verde_id)
-      references puntos_verdes (punto_verde_id)
+alter table tb_historial_negocio
+   add constraint FK_TB_HISTO_REFERENCE_TB_PUNTO foreign key (punto_verde_id)
+      references tb_puntos_verdes (punto_verde_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_negocio
-   add constraint FK_HISTORIA_REFERENCE_RECICLAJ foreign key (reciclaje_id)
-      references reciclaje (reciclaje_id)
+alter table tb_historial_negocio
+   add constraint FK_TB_HISTO_REFERENCE_TB_RECIC foreign key (reciclaje_id)
+      references tb_reciclaje (reciclaje_id)
       on delete restrict on update restrict;
 
-alter table historial_reciclaje_negocio
-   add constraint FK_HISTORIA_REFERENCE_NEGOCIO foreign key (negocio_id)
-      references negocio (negocio_id)
+alter table tb_historial_negocio
+   add constraint FK_TB_HISTO_REFERENCE_TB_NEGOC foreign key (negocio_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table ofertas
-   add constraint FK_OFERTAS_REFERENCE_NEGOCIO foreign key (negocio_id)
-      references negocio (negocio_id)
+alter table tb_ofertas
+   add constraint FK_TB_OFERT_REFERENCE_TB_NEGOC foreign key (negocio_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table puntos_verdes
-   add constraint FK_PUNTOS_V_REFERENCE_NEGOCIO foreign key (negocio_id)
-      references negocio (negocio_id)
+alter table tb_puntos_verdes
+   add constraint FK_TB_PUNTO_REFERENCE_TB_NEGOC foreign key (negocio_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table reciclaje
-   add constraint FK_RECICLAJ_REFERENCE_MATERIAL foreign key (material_id)
-      references materiales (materiales_id)
+alter table tb_reciclaje
+   add constraint FK_TB_RECIC_REFERENCE_TB_MATER foreign key (material_id)
+      references tb_materiales (materiales_id)
       on delete restrict on update restrict;
 
-alter table registra_reciclaje
-   add constraint FK_REGISTRA_REFERENCE_NEGOCIO foreign key (negocio_id)
-      references negocio (negocio_id)
+alter table tb_registra_reciclaje
+   add constraint FK_TB_REGIS_REFERENCE_TB_NEGOC foreign key (negocio_id)
+      references tb_negocio (negocio_id)
       on delete restrict on update restrict;
 
-alter table registra_reciclaje
-   add constraint FK_REGISTRA_REFERENCE_CIUDADAN foreign key (ciudadano_id)
-      references ciudadano (ciudadano_id)
+alter table tb_registra_reciclaje
+   add constraint FK_TB_REGIS_REFERENCE_TB_CIUDA foreign key (ciudadano_id)
+      references tb_ciudadano (ciudadano_id)
       on delete restrict on update restrict;
 
-alter table registra_reciclaje
-   add constraint FK_REGISTRA_REFERENCE_PUNTOS_V foreign key (punto_verde_id)
-      references puntos_verdes (punto_verde_id)
+alter table tb_registra_reciclaje
+   add constraint FK_TB_REGIS_REFERENCE_TB_PUNTO foreign key (punto_verde_id)
+      references tb_puntos_verdes (punto_verde_id)
       on delete restrict on update restrict;
 
-alter table registra_reciclaje
-   add constraint FK_REGISTRA_REFERENCE_RECICLAJ foreign key (reciclaje_id)
-      references reciclaje (reciclaje_id)
+alter table tb_registra_reciclaje
+   add constraint FK_TB_REGIS_REFERENCE_TB_RECIC foreign key (reciclaje_id)
+      references tb_reciclaje (reciclaje_id)
       on delete restrict on update restrict;
 
