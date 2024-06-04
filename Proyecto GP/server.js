309bceed53d47baab185ecaf2800f6c4c5bc2afe
AndyAllaica <151053665+AndyAllaica@users.noreply.github.com>
2024-06-03 22:17:32 -0500

    Cambios

--- Proyecto GP/server.js
+++ Proyecto GP/server.js
@@ -10,15 +10,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Incluir rutas CRUD
-const negocioRoutes = require('./routes/negocioRoutes');
-const ofertaRoutes = require('./routes/ofertaRoutes');
-const credencialRoutes = require('./routes/credencialRoutes');
-const puntoVerdeRoutes = require('./routes/puntoVerdeRoutes');
 
-app.use('/api/negocios', negocioRoutes);
-app.use('/api/ofertas', ofertaRoutes);
-app.use('/api/credenciales', credencialRoutes);
-app.use('/api/puntos_verdes', puntoVerdeRoutes);
+app.use('/api/negocios', require('./routes/negocioRoutes'));
+app.use('/api/ofertas', require('./routes/ofertaRoutes'));
+app.use('/api/credenciales', require('./routes/credencialRoutes'));
+app.use('/api/puntos_verdes', require('./routes/puntoVerdeRoutes'));
 app.use('/api/admins', require('./routes/admins'));
 app.use('/api/canjea_ofertas', require('./routes/canjeaOfertas'));
 app.use('/api/ciudadanos', require('./routes/ciudadanos'));
@@ -29,8 +25,6 @@ app.use('/api/materiales', require('./routes/materiales'));
 app.use('/api/reciclajes', require('./routes/reciclajes'));
 app.use('/api/registro_reciclaje', require('./routes/registroReciclajeRoutes'));
 
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
