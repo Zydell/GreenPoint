6c603ef367f940b817d17af6c61e1143db56b62c
AlexisByte <153880293+AlexisByte@users.noreply.github.com>
2024-06-03 19:38:38 -0500

    rutas api
    nuevas rutas

---
+++ Proyecto GP/routes/materiales.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+// Get all 
+router.get('/', async (req, res) => {
+  try {
+    const materiales = await db.tb_materiales.findAll();
+    res.status(200).json(materiales);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Create a new 
+router.post('/', async (req, res) => {
+  try {
+    const materiales = await db.tb_materiales.create(req.body);
+    res.status(201).json(materiales);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get an  by id
+router.get('/:id', async (req, res) => {
+  try {
+    const materiales = await db.tb_materiales.findByPk(req.params.id);
+    if (materiales) {
+      res.status(200).json(materiales);
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update an  by id
+router.put('/:id', async (req, res) => {
+  try {
+    const materiales = await db.tb_materiales.findByPk(req.params.id);
+    if (materiales) {
+      await materiales.update(req.body);
+      res.status(200).json(materiales);
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete an  by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const materiales = await db.tb_materiales.findByPk(req.params.id);
+    if (materiales) {
+      await materiales.destroy();
+      res.status(204).json();
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
