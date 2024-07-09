8f08e44c6cf2be98b8b8a971027945b727ffe09f
Cristhian <120286539+Zydell@users.noreply.github.com>
2024-07-09 15:38:12 -0500

    Act

--- GP - Backend/routes/ciudadanos.js
+++ GP - Backend/routes/ciudadanos.js
@@ -27,13 +27,54 @@ router.get('/:id', async (req, res) => {
   try {
     const ciudadanos = await db.tb_ciudadano.findByPk(req.params.id);
     if (ciudadanos) {
-      res.status(200).json(ciudadanos);
+      const credencial = await db.tb_credenciales.findOne({ 
+        where: { usuario_id: ciudadanos.ciudadano_id } });
+      const gc = await db.tb_greencoin_cdn.findOne({
+        where: { greencoin_id: ciudadanos.greencoin_id }
+      });
+      res.status(200).json({
+        ciudadano_id: ciudadanos.ciudadano_id,
+        correo_electronico: credencial.correo_electronico,
+        tipousuario: credencial.tipousuario,
+        nombre: ciudadanos.nombre,
+        apellido: ciudadanos.apellido,
+        telefono: ciudadanos.telefono,
+        fecha_nac: ciudadanos.fecha_nac,
+        estado: ciudadanos.estado,
+        greencoins: gc.total
+        }
+    ); 
+
     } else {
       res.status(404).json({ message: 'Not found' });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+
+  /*
+  userInfo = await tb_ciudadano.findOne({ where: { ciudadano_id: credencial.usuario_id } });
+
+            const gc = await tb_greencoin_cdn.findOne({
+                where: { greencoin_id: userInfo.greencoin_id }
+            });
+
+            res.json({ token ,
+                user: {
+                    ciudadano_id: credencial.usuario_id,
+                    correo_electronico: credencial.correo_electronico,
+                    tipousuario: credencial.tipousuario,
+                    nombre: userInfo.nombre,
+                    apellido: userInfo.apellido,
+                    telefono: userInfo.telefono,
+                    fecha_nac: userInfo.fecha_nac,
+                    estado: userInfo.estado,
+                    greencoins: gc.total
+                }
+            });
+  */
+
+
 });
 
 // Update an  by id
