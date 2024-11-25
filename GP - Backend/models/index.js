29710fc67f60a6ff1b861a1acdb2bd78edb6aec1
Cristhian <120286539+Zydell@users.noreply.github.com>
2024-11-25 18:10:08 -0500

    Prototipo sin conexion

--- GP - Backend/models/index.js
+++ GP - Backend/models/index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-//const env = process.env.NODE_ENV || 'development';      //PostgreSql
-const env = process.env.NODE_ENV || 'test';           //MySql
+const env = process.env.NODE_ENV || 'development';      //PostgreSql
+//const env = process.env.NODE_ENV || 'test';           //MySql
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
